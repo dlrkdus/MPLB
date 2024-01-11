3398502aced6875967d1d90c7f1bf9a67e183ff6
leegayeon <99721126+dlrkdus@users.noreply.github.com>
2024-01-11 20:05:37 +0900

    1/11 2장 및 깃허브 연동 완료

--- src/main/java/me/dlfkdu/MPLB/TestController.java
+++ src/main/java/me/dlfkdu/MPLB/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "Its just Test";
+        return "Its just Test!";
     }
 }

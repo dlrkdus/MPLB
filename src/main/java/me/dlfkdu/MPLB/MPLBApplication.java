08fb8f472ffab2353bf17d2d07350174979bad37
leegayeon <99721126+dlrkdus@users.noreply.github.com>
2024-01-11 19:42:58 +0900

    Initial commit

---
+++ src/main/java/me/dlfkdu/MPLB/MPLBApplication.java
@@ -0,0 +1,12 @@
+package me.dlfkdu.MPLB;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MPLBApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MPLBApplication.class,args);
+    }
+}

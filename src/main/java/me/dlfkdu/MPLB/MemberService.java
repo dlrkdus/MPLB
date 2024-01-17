d9bc72f37c7efcf197c967efe1c80d2d51a49b42
leegayeon <99721126+dlrkdus@users.noreply.github.com>
2024-01-17 18:20:32 +0900

    1/17 5장 완료

---
+++ src/main/java/me/dlfkdu/MPLB/MemberService.java
@@ -0,0 +1,23 @@
+package me.dlfkdu.MPLB;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    @Autowired
+    MemberRepository memberRepository;
+
+    public void test(){
+        //저장(Create)
+        memberRepository.save(new Member(1L,"A"));
+        //조회(Read)
+        Optional<Member> member=memberRepository.findById(1L);
+        List<Member> allMembers=memberRepository.findAll();
+        //삭제(Delete)
+        memberRepository.deleteById(1L);
+    }
+}
